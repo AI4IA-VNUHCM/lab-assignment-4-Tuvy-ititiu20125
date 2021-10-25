5e1d8ccee7a7a9bde41d11d19bb2b22abaaf201d
Tuvy-ititiu20125 <ITITIU20125@student.hcmiu.edu.vn>
2021-10-25 15:35:34 +0700

    update ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -13,17 +13,41 @@ ______________________________________
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-void Ex1(int n){
-	//Your codes here
-    
+void Ex1(int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			int iF=1;
+			for (int k = 1; k<=i; k++)
+			{
+				iF=iF*k;
+			}
+			int jF=1;
+			for (int k = 1; k<=j; k++)
+			{
+				jF=jF*k;
+			}
+			int ijF=1;
+			for (int k = 1; k <= i-j; k++)
+			{
+				ijF=ijF*k;
+			}
+			int C = iF/(ijF*jF);
+			printf ("%4d",C);
+		}
+		printf ("\n");
+	}
+	
 }
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
+int main(int argc, char *argv[])
+{
+	// testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
-	
+
 	Ex1(testcase);
 	return 0;
 }
